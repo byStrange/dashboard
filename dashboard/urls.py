be1d814b268fe5e63707bc2c82ad4452976800bc
byStrange <qosimovrahmatullo006@gmail.com>
2022-07-25 20:01:58 +0500

    some changes

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -15,13 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-# static files
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path('', lambda request: redirect('/my/', permanent=False), name='dashboard'),
     path('my/', include('her.urls', namespace="her")),
 ]
 
